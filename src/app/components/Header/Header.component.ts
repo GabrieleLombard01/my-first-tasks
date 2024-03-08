35ff990ad03bc5fd5b319d3a36233b2d4402a853
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2024-03-08 12:23:04 +0100

    header add

---
+++ src/app/components/Header/Header.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Header',
+  templateUrl: './Header.component.html',
+  styleUrls: ['./Header.component.css'],
+  standalone: true,
+
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
