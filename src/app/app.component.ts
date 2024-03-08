05b39ca0cd5c01f8e0e308146f3e87c150911cc4
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2024-03-08 14:55:33 +0100

    gallery component add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/Header/Header.component';
+import { GalleryComponent } from './components/Gallery/Gallery.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, GalleryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/Gallery/Gallery.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Gallery',
+  templateUrl: './Gallery.component.html',
+  styleUrls: ['./Gallery.component.css'],
+  standalone: true,
+})
+export class GalleryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
