4493fe7f0942848c19caa6280a2ab25a640cce95
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2024-03-08 17:59:27 +0100

    footer add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/Header/Header.component';
 import { GalleryComponent } from './components/Gallery/Gallery.component';
+import { FooterComponent } from './components/Footer/Footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, GalleryComponent],
+  imports: [HeaderComponent, GalleryComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
